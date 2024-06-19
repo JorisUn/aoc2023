82b8a0a0415ddb6af0ce335336164a4d124d6f3b
JorisUn <joris.unikas21@gmail.com>
2024-06-19 18:50:07 +0300

    --allow-empty

--- day4/part2.cpp
+++ day4/part2.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cctype>
 #include <fstream>
 #include <iostream>
@@ -17,20 +18,24 @@ class Card{
             }
         }
         int findWinners(){
-            int k=0;
+            int n=0;
             for(int i=0;i<winSize;i++){
                 for(int j=0;j<haveSize;j++){
                     if(winNum[i] == haveNum[j])
-                        k++;
+                        n++;
                 }
             }
-            return k;
+            return n;
         }
     public:
         int idx{-1};
         Card(std::string input):
             line{input}
         {}
+        Card(){}
+        void setLine(std::string ln){
+            line = ln;
+        }
         int getWinSize(){
             return winSize;
         }
@@ -53,19 +58,55 @@ class Card{
             return findWinners();
         }
 };
+int columnSum(int column[], int i);
+void calcWeights(Card A[], int n);
 std::ifstream in("input.txt");
+std::ofstream out("results.txt");
 
 int main (int argc, char *argv[]) {
     std::string line{};
-    [[maybe_unused]] int sum{0}, k{0};
+    Card A[200];
+    [[maybe_unused]] int sum{0}, n{0};
     while (std::getline(in, line)){
-        Card A = Card(line);
-        A.idx = k;
-        k++;
-        A.parseInput();
-        sum+= A.calcPoints();
+        A[n] = Card(line);
+        A[n].idx = n;
+        A[n].parseInput();
+        n++;
     }
-    std::cout << sum << '\n';
+
+    calcWeights(A, n);
+}
+int countWin(Card A[], int n){
+    for(int i=0;i<n;i++){
+        A[i].winningNumber();
+    }
+    return 0;
+}
+void calcWeights(Card A[], int n){
+    int valuesA[201][200];
+    //Clearing matrix;
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<n;j++){
+            valuesA[i][j]=0;
+        }
+    }
+    for(int j=0;j<n;j++)
+        valuesA[0][j] = 1;
+    for(int i=1;i<n+1;i++){
+        for(int j=i;j<i+A[i-1].winningNumber();j++){
+            int sum{0};
+            for(int k=0;k<i;k++)
+                sum+=valuesA[k][j-1];
+            valuesA[i][j] = sum;
+        }
+    }
+    int sum = 0;
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<n;j++){
+           sum+=valuesA[i][j];
+        }
+    }
+    std::cout << sum+198 << '\n';
 }
 
 
